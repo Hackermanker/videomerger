1a0478814a81b208488c7f0dd3e8cc51bff8b25a
ROGUE <88287526+Hackermanker@users.noreply.github.com>
2023-01-18 16:10:49 +0530

    Update ffmpeg.py

--- helpers/ffmpeg.py
+++ helpers/ffmpeg.py
@@ -18,7 +18,7 @@ async def MergeVideo(input_file: str, user_id: int, message: Message, format_: s
     :return: This will return Merged Video File Path
     """
 
-    output_vid = f"{Config.DOWN_PATH}/{str(user_id)}/[@AbirHasan2005]_Merged.{format_.lower()}"
+    output_vid = f"{Config.DOWN_PATH}/{str(user_id)}/[@Oriksonic]_Merged.{format_.lower()}"
     file_generator_command = [
         "ffmpeg",
         "-f",
